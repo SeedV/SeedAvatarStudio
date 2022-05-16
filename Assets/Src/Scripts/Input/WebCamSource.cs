fdb374aaa5da1b61bac87efa7c6b7d46d8557b3a
superkudo <superkudo@163.com>
2022-05-16 16:55:46 +0800

    Fixed some code style and review recommend

--- Assets/Src/Scripts/Input/WebCamSource.cs
+++ Assets/Src/Scripts/Input/WebCamSource.cs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+using System;
 using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
@@ -19,140 +19,188 @@ using UnityEngine;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
 
-public class WebCamSource : MonoBehaviour {
-  public Dropdown CameraDropdown;
-  public Dropdown ResolutionDropdown;
-  public Dropdown FpsDropdown;
-  public Dropdown FlipDropdown;
-  public string DeviceName;
-  public int Fps = 30;
-  public bool Flip = false;
-  [SerializeField] private RenderTexture _buffer;
-  private WebCamTexture _webCam;
-  private int _width = 1280;
-  private int _height = 720;
-  private List<string> _devicesName = new List<string>();
-  private List<string> _resolutionOpt = new List<string>(){"1280X720","1600X900","1920X1080"};
-  private List<string> _fpsOpt = new List<string>(){"30Hz","60Hz"};
-  private List<string> _flipOpt = new List<string>(){"默认","水平翻转"};
-  //Start button event
-  public void Play() {
-    if(_webCam == null) {
-      _webCam = new WebCamTexture(DeviceName, _width, _height, Fps);
-      _webCam.Play();
-    }
+namespace SeedAvatar {
+  public enum DropdownKey {
+    CAMERA = 0,
+    RESOLUTION = 1,
+    FPS = 2,
+    FLIP = 3
   }
-  //Stop button event
-  public void Stop() {
-    if(_webCam != null) {
-      _webCam.Stop();
-      _webCam = null;
-    }
+  public enum ResolutionOptions {
+    LOW = 0,
+    MIDDLE = 1,
+    HIGH = 2,
   }
-  //DropdownChange event
-  public void CameraDropdownChange(int value) {
-    //Debug.Log(_devicesName.ToArray()[value]);
-    if(_devicesName.ToArray()[value] != null) {
-      DeviceName = _devicesName.ToArray()[value];
-    }
+  public enum FpsOptions {
+    LOW = 0,
+    HiGH = 1
   }
-  //DropdownChange event
-  public void ResolutionDropdownChange(int value) {
-    string _resolution = _resolutionOpt.ToArray()[value];
-    if(_resolution != null) {
-      switch(_resolution) {
-        case "1280X720":
+  public enum FlipOptions {
+    NORMAL = 0,
+    MIRRORING = 1
+  }
+  public class WebCamSource : MonoBehaviour {
+    public Dropdown CameraDropdown;
+    public Dropdown ResolutionDropdown;
+    public Dropdown FpsDropdown;
+    public Dropdown FlipDropdown;
+    public string DeviceName;
+    public int Fps = 30;
+    public bool Flip = false;
+    [SerializeField] private RenderTexture _buffer;
+    private WebCamTexture _webCam;
+    private int _width = 1280;
+    private int _height = 720;
+    private static readonly Dictionary<ResolutionOptions, string> _resolutionDic =
+    new Dictionary<ResolutionOptions, string>
+    {
+      { ResolutionOptions.LOW , "1280x720" } , {  ResolutionOptions.MIDDLE, "1600x900" } , { ResolutionOptions.HIGH , "1920x1080" }
+    };
+    private static readonly Dictionary<FpsOptions, string> _fpsDic =
+    new Dictionary<FpsOptions, string>
+    {
+      { FpsOptions.LOW , "30Hz" } , { FpsOptions.HiGH , "60Hz" }
+    };
+    private static readonly Dictionary<FlipOptions, string> _flipDic =
+    new Dictionary<FlipOptions, string>
+    {
+      { FlipOptions.NORMAL , "Normal" } , { FlipOptions.MIRRORING , "Mirroring" }
+    };
+    private List<string> _devicesName = new List<string>();
+    private List<string> _resolutionOpt = new List<string>();
+    private List<string> _fpsOpt = new List<string>();
+    private List<string> _flipOpt = new List<string>();
+
+    // Start button event.
+    public void Play() {
+      if (_webCam == null) {
+        _webCam = new WebCamTexture(DeviceName, _width, _height, Fps);
+        _webCam.Play();
+      }
+    }
+
+    // Stop button event.
+    public void Stop() {
+      if (_webCam != null) {
+        _webCam.Stop();
+        _webCam = null;
+      }
+    }
+
+    // DropdownChange event.
+    public void CameraDropdownChange(int value) {
+      //Debug.Log(_devicesName.ToArray()[value]);
+      if (_devicesName.ToArray()[value] != null) {
+        DeviceName = _devicesName.ToArray()[value];
+      }
+    }
+
+    // DropdownChange event.
+    public void ResolutionDropdownChange(int value) {
+      switch (value) {
+        case (int)ResolutionOptions.LOW:
           _width = 1280;
           _height = 720;
           break;
-        case "1600X900":
+        case (int)ResolutionOptions.MIDDLE:
           _width = 1600;
           _height = 900;
           break;
-        case "1920X1080":
+        case (int)ResolutionOptions.HIGH:
           _width = 1920;
           _height = 1080;
           break;
         default:
           break;
       }
-      //Debug.Log(_width);
     }
-  }
-  //DropdownChange event
-  public void FpsDropdownChange(int value) {
-    string _fps = _fpsOpt.ToArray()[value];
-    if(_fps != null) {
-      switch(_fps) {
-        case "60Hz":
+
+    // DropdownChange event.
+    public void FpsDropdownChange(int value) {
+      switch (value) {
+        case (int)FpsOptions.LOW:
           Fps = 60;
           break;
-        case "30Hz":
+        case (int)FpsOptions.HiGH:
           Fps = 30;
           break;
         default:
           break;
       }
-      //Debug.Log(Fps);
     }
-  }
-  //DropdownChange event
-  public void FlipDropdownChange(int value) {
-    string _flip = _flipOpt.ToArray()[value];
-    if(_flip != null) {
-      switch(_flip) {
-        case "默认":
+
+    // DropdownChange event.
+    public void FlipDropdownChange(int value) {
+      switch (value) {
+        case (int)FlipOptions.NORMAL:
           Flip = false;
           break;
-        case "水平翻转":
+        case (int)FlipOptions.MIRRORING:
           Flip = true;
           break;
         default:
           break;
       }
-      //Debug.Log(Flip);
     }
-  }
-  //Webcam and params init
-  IEnumerator Start() {
-    yield return Application.RequestUserAuthorization (UserAuthorization.WebCam);
-    if(Application.HasUserAuthorization(UserAuthorization.WebCam)) {
-      WebCamDevice[] _devices = WebCamTexture.devices;
-      CameraDropdown.ClearOptions();
-      for(int i = 0; i< _devices.Length; i++) {
-        _devicesName.Add(_devices[i].name);
+
+    private void InitDropdown(Dropdown target, List<string> optarr, DropdownKey dropdownkey) {
+      target.ClearOptions();
+      switch (dropdownkey) {
+        case DropdownKey.CAMERA:
+          target.AddOptions(optarr);
+          target.onValueChanged.AddListener(CameraDropdownChange);
+          break;
+        case DropdownKey.RESOLUTION:
+          foreach (ResolutionOptions opt in Enum.GetValues(typeof(ResolutionOptions))) {
+            optarr.Add(_resolutionDic[opt]);
+          }
+          target.AddOptions(optarr);
+          target.onValueChanged.AddListener(ResolutionDropdownChange);
+          break;
+        case DropdownKey.FPS:
+          foreach (FpsOptions opt in Enum.GetValues(typeof(FpsOptions))) {
+            optarr.Add(_fpsDic[opt]);
+          }
+          target.AddOptions(optarr);
+          target.onValueChanged.AddListener(FpsDropdownChange);
+          break;
+        case DropdownKey.FLIP:
+          foreach (FlipOptions opt in Enum.GetValues(typeof(FlipOptions))) {
+            optarr.Add(_flipDic[opt]);
+          }
+          target.AddOptions(optarr);
+          target.onValueChanged.AddListener(FlipDropdownChange);
+          break;
+        default:
+          break;
       }
-      CameraDropdown.AddOptions(_devicesName);
-      CameraDropdown.value = 0;
-      CameraDropdown.captionText.text = CameraDropdown.options[0].text;
-      CameraDropdown.onValueChanged.AddListener(CameraDropdownChange);
+      target.value = 0;
+      target.captionText.text = target.options[0].text;
     }
 
-    ResolutionDropdown.ClearOptions();
-    ResolutionDropdown.AddOptions(_resolutionOpt);
-    ResolutionDropdown.value = 0;
-    ResolutionDropdown.captionText.text = ResolutionDropdown.options[0].text;
-    ResolutionDropdown.onValueChanged.AddListener(ResolutionDropdownChange);
+    // Webcam params init.
+    IEnumerator Start() {
+      yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+      if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+        WebCamDevice[] _devices = WebCamTexture.devices;
+        foreach (var device in _devices) {
+          _devicesName.Add(device.name);
+        }
+        InitDropdown(CameraDropdown, _devicesName, DropdownKey.CAMERA);
+      }
+      InitDropdown(ResolutionDropdown, _resolutionOpt, DropdownKey.RESOLUTION);
+      InitDropdown(FpsDropdown, _fpsOpt, DropdownKey.FPS);
+      InitDropdown(FlipDropdown, _flipOpt, DropdownKey.FLIP);
+    }
 
-    FpsDropdown.ClearOptions();
-    FpsDropdown.AddOptions(_fpsOpt);
-    FpsDropdown.value = 0;
-    FpsDropdown.captionText.text = FpsDropdown.options[0].text;
-    FpsDropdown.onValueChanged.AddListener(FpsDropdownChange);
-
-    FlipDropdown.ClearOptions();
-    FlipDropdown.AddOptions(_flipOpt);
-    FlipDropdown.value = 0;
-    FlipDropdown.captionText.text = FlipDropdown.options[0].text;
-    FlipDropdown.onValueChanged.AddListener(FlipDropdownChange);
-  }
-  void Update() {
-    if(_webCam != null) {
-      if (!_webCam.didUpdateThisFrame) return;
-      bool vFlip = _webCam.videoVerticallyMirrored;
-      Vector2 scale = new Vector2(Flip ? -1 : 1, vFlip ? -1 : 1);
-      Vector2 offset = new Vector2(Flip ? 1 : 0 / 2, vFlip ? 1 : 0);
-      Graphics.Blit(_webCam, _buffer, scale, offset);
+    void Update() {
+      if (_webCam != null) {
+        if (!_webCam.didUpdateThisFrame) return;
+        bool vFlip = _webCam.videoVerticallyMirrored;
+        Vector2 scale = new Vector2(Flip ? -1 : 1, vFlip ? -1 : 1);
+        Vector2 offset = new Vector2(Flip ? 1 : 0, vFlip ? 1 : 0);
+        Graphics.Blit(_webCam, _buffer, scale, offset);
+      }
     }
   }
 }
