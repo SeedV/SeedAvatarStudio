1a0aceb9d57c6fea9538a8ab7ab716e17f352416
superkudo <superkudo@163.com>
2022-05-17 13:06:01 +0800

    Fixed some review recommends

--- Assets/Src/Scripts/SeedAvatar/Input/WebCamSource.cs
+++ Assets/Src/Scripts/SeedAvatar/Input/WebCamSource.cs
@@ -11,34 +11,33 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 using System;
 using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using Debug = UnityEngine.Debug;
 
 namespace SeedAvatar {
-  public enum DropdownKey {
-    CAMERA = 0,
-    RESOLUTION = 1,
-    FPS = 2,
-    FLIP = 3
-  }
   public enum ResolutionOptions {
     LOW = 0,
     MIDDLE = 1,
     HIGH = 2,
   }
+
   public enum FpsOptions {
     LOW = 0,
-    HiGH = 1
+    HIGH = 1,
   }
+
   public enum FlipOptions {
     NORMAL = 0,
-    MIRRORING = 1
+    MIRRORING = 1,
   }
+
   public class WebCamSource : MonoBehaviour {
     public Dropdown CameraDropdown;
     public Dropdown ResolutionDropdown;
@@ -51,25 +50,23 @@ namespace SeedAvatar {
     private WebCamTexture _webCam;
     private int _width = 1280;
     private int _height = 720;
-    private static readonly Dictionary<ResolutionOptions, string> _resolutionDic =
-    new Dictionary<ResolutionOptions, string>
-    {
-      { ResolutionOptions.LOW , "1280x720" } , {  ResolutionOptions.MIDDLE, "1600x900" } , { ResolutionOptions.HIGH , "1920x1080" }
+    private static readonly Dictionary<int, string> _resolutionDict =
+    new Dictionary<int, string> {
+      { (int)ResolutionOptions.LOW , "1280x720" },
+      { (int)ResolutionOptions.MIDDLE, "1600x900" },
+      { (int)ResolutionOptions.HIGH , "1920x1080" },
     };
-    private static readonly Dictionary<FpsOptions, string> _fpsDic =
-    new Dictionary<FpsOptions, string>
-    {
-      { FpsOptions.LOW , "30Hz" } , { FpsOptions.HiGH , "60Hz" }
+    private static readonly Dictionary<int, string> _fpsDict =
+    new Dictionary<int, string> {
+      { (int)FpsOptions.LOW , "30Hz" },
+      { (int)FpsOptions.HIGH , "60Hz" },
     };
-    private static readonly Dictionary<FlipOptions, string> _flipDic =
-    new Dictionary<FlipOptions, string>
-    {
-      { FlipOptions.NORMAL , "Normal" } , { FlipOptions.MIRRORING , "Mirroring" }
+    private static readonly Dictionary<int, string> _flipDict =
+    new Dictionary<int, string> {
+      { (int)FlipOptions.NORMAL , "Normal" },
+      { (int)FlipOptions.MIRRORING , "Mirroring" },
     };
-    private List<string> _devicesName = new List<string>();
-    private List<string> _resolutionOpt = new List<string>();
-    private List<string> _fpsOpt = new List<string>();
-    private List<string> _flipOpt = new List<string>();
+    private Dictionary<int, string> _devicesDict = new Dictionary<int, string> { };
 
     // Start button event.
     public void Play() {
@@ -88,15 +85,14 @@ namespace SeedAvatar {
     }
 
     // DropdownChange event.
-    public void CameraDropdownChange(int value) {
-      //Debug.Log(_devicesName.ToArray()[value]);
-      if (_devicesName.ToArray()[value] != null) {
-        DeviceName = _devicesName.ToArray()[value];
+    public void OnCameraDropdownChange(int value) {
+      if (_devicesDict[value] != null) {
+        DeviceName = _devicesDict[value];
       }
     }
 
     // DropdownChange event.
-    public void ResolutionDropdownChange(int value) {
+    public void OnResolutionDropdownChange(int value) {
       switch (value) {
         case (int)ResolutionOptions.LOW:
           _width = 1280;
@@ -116,12 +112,12 @@ namespace SeedAvatar {
     }
 
     // DropdownChange event.
-    public void FpsDropdownChange(int value) {
+    public void OnFpsDropdownChange(int value) {
       switch (value) {
         case (int)FpsOptions.LOW:
           Fps = 60;
           break;
-        case (int)FpsOptions.HiGH:
+        case (int)FpsOptions.HIGH:
           Fps = 30;
           break;
         default:
@@ -130,7 +126,7 @@ namespace SeedAvatar {
     }
 
     // DropdownChange event.
-    public void FlipDropdownChange(int value) {
+    public void OnFlipDropdownChange(int value) {
       switch (value) {
         case (int)FlipOptions.NORMAL:
           Flip = false;
@@ -143,59 +139,39 @@ namespace SeedAvatar {
       }
     }
 
-    private void InitDropdown(Dropdown target, List<string> optarr, DropdownKey dropdownkey) {
+    private void InitDropdown(Dropdown target, Dictionary<int, string> optDict, UnityAction<int> onChangedCallback) {
       target.ClearOptions();
-      switch (dropdownkey) {
-        case DropdownKey.CAMERA:
-          target.onValueChanged.AddListener(CameraDropdownChange);
-          break;
-        case DropdownKey.RESOLUTION:
-          foreach (ResolutionOptions opt in Enum.GetValues(typeof(ResolutionOptions))) {
-            optarr.Add(_resolutionDic[opt]);
-          }
-          target.onValueChanged.AddListener(ResolutionDropdownChange);
-          break;
-        case DropdownKey.FPS:
-          foreach (FpsOptions opt in Enum.GetValues(typeof(FpsOptions))) {
-            optarr.Add(_fpsDic[opt]);
-          }
-          target.onValueChanged.AddListener(FpsDropdownChange);
-          break;
-        case DropdownKey.FLIP:
-          foreach (FlipOptions opt in Enum.GetValues(typeof(FlipOptions))) {
-            optarr.Add(_flipDic[opt]);
-          }
-          target.onValueChanged.AddListener(FlipDropdownChange);
-          break;
-        default:
-          break;
-      }
-      target.AddOptions(optarr);
+      List<string> optStrings = optDict.Count == 0 ? new List<string>() : new List<string>(optDict.Values);
+      target.AddOptions(optStrings);
       target.value = 0;
       target.captionText.text = target.options[0].text;
+      target.onValueChanged.AddListener(onChangedCallback);
     }
 
     // Webcam params init.
     IEnumerator Start() {
       yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
       if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
-        WebCamDevice[] _devices = WebCamTexture.devices;
-        foreach (var device in _devices) {
-          _devicesName.Add(device.name);
+        WebCamDevice[] devices = WebCamTexture.devices;
+        int dictKey = 0;
+        _devicesDict.Clear();
+        foreach (var device in devices) {
+          _devicesDict.Add(dictKey, device.name);
+          _dictKey++;
         }
-        InitDropdown(CameraDropdown, _devicesName, DropdownKey.CAMERA);
+        InitDropdown(CameraDropdown, _devicesDict, OnCameraDropdownChange);
       }
-      InitDropdown(ResolutionDropdown, _resolutionOpt, DropdownKey.RESOLUTION);
-      InitDropdown(FpsDropdown, _fpsOpt, DropdownKey.FPS);
-      InitDropdown(FlipDropdown, _flipOpt, DropdownKey.FLIP);
+      InitDropdown(ResolutionDropdown, _resolutionDict, OnResolutionDropdownChange);
+      InitDropdown(FpsDropdown, _fpsDict, OnFpsDropdownChange);
+      InitDropdown(FlipDropdown, _flipDict, OnFlipDropdownChange);
     }
 
     void Update() {
       if (_webCam != null) {
         if (!_webCam.didUpdateThisFrame) return;
         bool vFlip = _webCam.videoVerticallyMirrored;
-        Vector2 scale = new Vector2(Flip ? -1 : 1, vFlip ? -1 : 1);
-        Vector2 offset = new Vector2(Flip ? 1 : 0, vFlip ? 1 : 0);
+        var scale = new Vector2(Flip ? -1 : 1, vFlip ? -1 : 1);
+        var offset = new Vector2(Flip ? 1 : 0, vFlip ? 1 : 0);
         Graphics.Blit(_webCam, _buffer, scale, offset);
       }
     }
---
+++ Assets/Src/Scripts/SeedAvatar/UI/HeaderButtonEvents.cs
@@ -0,0 +1,68 @@
+// Copyright 2021-2022 The SeedV Lab.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SeedAvatar {
+  // This class is the UI header panel buttons click events script.
+  public class HeaderButtonEvents : MonoBehaviour {
+
+    // Animator trigger Param.
+    private const string _animatorShow = "Show";
+    // Animator trigger Param.
+    private const string _animatorHide = "Hide";
+    [SerializeField] private GameObject _inputDialog;
+    // Header panel animator.
+    [SerializeField] private Animator _animatorHeader;
+    // Viewport panel animator.
+    [SerializeField] private Animator _animatorViewport;
+
+    // HideUI button click event.
+    public void OnHideUI() {
+      _animatorHeader.SetTrigger(_animatorHide);
+      _animatorViewport.SetTrigger(_animatorHide);
+    }
+
+    // ExithideUI button click event.
+    public void OnShowUI() {
+      _animatorHeader.SetTrigger(_animatorShow);
+      _animatorViewport.SetTrigger(_animatorShow);
+    }
+
+    // Close app button click event.
+    public void OnClose() {
+      Application.Quit();
+    }
+
+    // Play button click event.
+    public void OnPlay() {
+    }
+
+    // Stop button click event.
+    public void OnStop() {
+    }
+
+    // Setting button click event.
+    public void OnSetting() {
+    }
+
+    // InputSetting button click event.
+    public void OnInputSet() {
+      _inputDialog.SetActive(true);
+    }
+  }
+}
