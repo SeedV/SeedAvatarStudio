7c2979af2398bc5788f6a4bc863fd4c9cd59107f
superkudo <superkudo@163.com>
2022-06-01 16:51:20 +0800

    Add fps viewport script

---
+++ Assets/Src/Scripts/SeedAvatar/UI/FpsViewport.cs
@@ -0,0 +1,43 @@
+// Copyright 2021-2022 The SeedV Lab.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SeedAvatar {
+  public class FpsViewport : MonoBehaviour {
+    public Text FpsLabel;
+    private float _updateInterval = 0.5F;
+    private float _lastInterval;
+    private int _frameCount;
+
+    void Start() {
+      _lastInterval = Time.realtimeSinceStartup;
+      _frameCount = 0;
+    }
+
+    void Update() {
+      ++_frameCount;
+      if (Time.realtimeSinceStartup > _lastInterval + _updateInterval) {
+        float fps = _frameCount / (Time.realtimeSinceStartup - _lastInterval);
+        _frameCount = 0;
+        _lastInterval = Time.realtimeSinceStartup;
+        FpsLabel.text = "fps:" + fps.ToString("f2");//#0.00
+        FpsLabel.color = fps >= 20 ? Color.green : (fps > 15 ? Color.yellow : Color.red);
+      }
+    }
+  }
+}
