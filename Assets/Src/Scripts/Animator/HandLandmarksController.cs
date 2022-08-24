3a6f61dc85d3e9649cb0db019f913a7254b71555
superkudo <superkudo@163.com>
2022-08-24 11:49:01 +0800

    Fixed the finger animation

--- Assets/Src/Scripts/Animator/HandLandmarksController.cs
+++ Assets/Src/Scripts/Animator/HandLandmarksController.cs
@@ -30,7 +30,8 @@ namespace SeedUnityVRKit {
     public int ScreenWidth;
     public int ScreenHeight;
     public NormalizedLandmarkList HandLandmarkList { private get; set; }
-    public Quaternion InitRotation = Quaternion.Euler(0, -90, -90);
+    public Quaternion InitWristRotation = Quaternion.Euler(-90, 90, 0);
+    public Quaternion InitFingerRotation = Quaternion.Euler(90, 0, 0);
 
     // Total number of landmarks in HandPose model, per hand.
     private const int _landmarksNum = 21;
@@ -58,22 +59,6 @@ namespace SeedUnityVRKit {
       _screenRatio = 1.0f * ScreenWidth / ScreenHeight;
 
       if (handType == HandType.LeftHand) {
-        _fingerTargets[0] = anim.GetBoneTransform(HumanBodyBones.LeftThumbProximal);
-        _fingerTargets[1] = anim.GetBoneTransform(HumanBodyBones.LeftThumbIntermediate);
-        _fingerTargets[2] = anim.GetBoneTransform(HumanBodyBones.LeftThumbDistal);
-        _fingerTargets[3] = anim.GetBoneTransform(HumanBodyBones.LeftIndexProximal);
-        _fingerTargets[4] = anim.GetBoneTransform(HumanBodyBones.LeftIndexIntermediate);
-        _fingerTargets[5] = anim.GetBoneTransform(HumanBodyBones.LeftIndexDistal);
-        _fingerTargets[6] = anim.GetBoneTransform(HumanBodyBones.LeftMiddleProximal);
-        _fingerTargets[7] = anim.GetBoneTransform(HumanBodyBones.LeftMiddleIntermediate);
-        _fingerTargets[8] = anim.GetBoneTransform(HumanBodyBones.LeftMiddleDistal);
-        _fingerTargets[9] = anim.GetBoneTransform(HumanBodyBones.LeftRingProximal);
-        _fingerTargets[10] = anim.GetBoneTransform(HumanBodyBones.LeftRingIntermediate);
-        _fingerTargets[11] = anim.GetBoneTransform(HumanBodyBones.LeftRingDistal);
-        _fingerTargets[12] = anim.GetBoneTransform(HumanBodyBones.LeftLittleProximal);
-        _fingerTargets[13] = anim.GetBoneTransform(HumanBodyBones.LeftLittleIntermediate);
-        _fingerTargets[14] = anim.GetBoneTransform(HumanBodyBones.LeftLittleDistal);
-      } else {
         _fingerTargets[0] = anim.GetBoneTransform(HumanBodyBones.RightThumbProximal);
         _fingerTargets[1] = anim.GetBoneTransform(HumanBodyBones.RightThumbIntermediate);
         _fingerTargets[2] = anim.GetBoneTransform(HumanBodyBones.RightThumbDistal);
@@ -89,13 +74,29 @@ namespace SeedUnityVRKit {
         _fingerTargets[12] = anim.GetBoneTransform(HumanBodyBones.RightLittleProximal);
         _fingerTargets[13] = anim.GetBoneTransform(HumanBodyBones.RightLittleIntermediate);
         _fingerTargets[14] = anim.GetBoneTransform(HumanBodyBones.RightLittleDistal);
+      } else {
+        _fingerTargets[0] = anim.GetBoneTransform(HumanBodyBones.LeftThumbProximal);
+        _fingerTargets[1] = anim.GetBoneTransform(HumanBodyBones.LeftThumbIntermediate);
+        _fingerTargets[2] = anim.GetBoneTransform(HumanBodyBones.LeftThumbDistal);
+        _fingerTargets[3] = anim.GetBoneTransform(HumanBodyBones.LeftIndexProximal);
+        _fingerTargets[4] = anim.GetBoneTransform(HumanBodyBones.LeftIndexIntermediate);
+        _fingerTargets[5] = anim.GetBoneTransform(HumanBodyBones.LeftIndexDistal);
+        _fingerTargets[6] = anim.GetBoneTransform(HumanBodyBones.LeftMiddleProximal);
+        _fingerTargets[7] = anim.GetBoneTransform(HumanBodyBones.LeftMiddleIntermediate);
+        _fingerTargets[8] = anim.GetBoneTransform(HumanBodyBones.LeftMiddleDistal);
+        _fingerTargets[9] = anim.GetBoneTransform(HumanBodyBones.LeftRingProximal);
+        _fingerTargets[10] = anim.GetBoneTransform(HumanBodyBones.LeftRingIntermediate);
+        _fingerTargets[11] = anim.GetBoneTransform(HumanBodyBones.LeftRingDistal);
+        _fingerTargets[12] = anim.GetBoneTransform(HumanBodyBones.LeftLittleProximal);
+        _fingerTargets[13] = anim.GetBoneTransform(HumanBodyBones.LeftLittleIntermediate);
+        _fingerTargets[14] = anim.GetBoneTransform(HumanBodyBones.LeftLittleDistal);
       }
     }
 
     async void Update() {
       if (HandLandmarkList != null) {
         transform.position = _target.transform.position;
-        _target.rotation = ComputeWristRotation() * InitRotation;
+        _target.rotation = ComputeWristRotation() * InitWristRotation;
         NormalizedLandmark landmark0 = HandLandmarkList.Landmark[0];
         NormalizedLandmark landmark1 = HandLandmarkList.Landmark[1];
         var s = _modelThumbLength / (ToVector(landmark1) - ToVector(landmark0)).magnitude;
@@ -107,39 +108,39 @@ namespace SeedUnityVRKit {
         }
 
         _fingerDirections[0] = _handLandmarks[2].transform.position - _handLandmarks[1].transform.position;
-        _fingerTargets[0].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[0].x, _fingerDirections[0].y, -_fingerDirections[0].z));
+        _fingerTargets[0].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[0].x, _fingerDirections[0].y, -_fingerDirections[0].z));
         _fingerDirections[1] = _handLandmarks[3].transform.position - _handLandmarks[2].transform.position;
-        _fingerTargets[1].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[1].x, _fingerDirections[1].y, -_fingerDirections[1].z));
+        _fingerTargets[1].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[1].x, _fingerDirections[1].y, -_fingerDirections[1].z));
         _fingerDirections[2] = _handLandmarks[4].transform.position - _handLandmarks[3].transform.position;
-        _fingerTargets[2].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[2].x, _fingerDirections[2].y, -_fingerDirections[2].z));
+        _fingerTargets[2].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[2].x, _fingerDirections[2].y, -_fingerDirections[2].z));
 
         _fingerDirections[3] = _handLandmarks[6].transform.position - _handLandmarks[5].transform.position;
-        _fingerTargets[3].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[3].x, _fingerDirections[3].y, _fingerDirections[3].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[3].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[3].x, _fingerDirections[3].y, _fingerDirections[3].z)) * InitFingerRotation;
         _fingerDirections[4] = _handLandmarks[7].transform.position - _handLandmarks[6].transform.position;
-        _fingerTargets[4].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[4].x, _fingerDirections[4].y, _fingerDirections[4].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[4].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[4].x, _fingerDirections[4].y, _fingerDirections[4].z)) * InitFingerRotation;
         _fingerDirections[5] = _handLandmarks[8].transform.position - _handLandmarks[7].transform.position;
-        _fingerTargets[5].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[5].x, _fingerDirections[5].y, _fingerDirections[5].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[5].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[5].x, _fingerDirections[5].y, _fingerDirections[5].z)) * InitFingerRotation;
 
         _fingerDirections[6] = _handLandmarks[10].transform.position - _handLandmarks[9].transform.position;
-        _fingerTargets[6].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[6].x, _fingerDirections[6].y, _fingerDirections[6].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[6].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[6].x, _fingerDirections[6].y, _fingerDirections[6].z)) * InitFingerRotation;
         _fingerDirections[7] = _handLandmarks[11].transform.position - _handLandmarks[10].transform.position;
-        _fingerTargets[7].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[7].x, _fingerDirections[7].y, _fingerDirections[7].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[7].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[7].x, _fingerDirections[7].y, _fingerDirections[7].z)) * InitFingerRotation;
         _fingerDirections[8] = _handLandmarks[12].transform.position - _handLandmarks[11].transform.position;
-        _fingerTargets[8].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[8].x, _fingerDirections[8].y, _fingerDirections[8].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[8].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[8].x, _fingerDirections[8].y, _fingerDirections[8].z)) * InitFingerRotation;
 
         _fingerDirections[9] = _handLandmarks[14].transform.position - _handLandmarks[13].transform.position;
-        _fingerTargets[9].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[9].x, _fingerDirections[9].y, _fingerDirections[9].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[9].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[9].x, _fingerDirections[9].y, _fingerDirections[9].z)) * InitFingerRotation;
         _fingerDirections[10] = _handLandmarks[15].transform.position - _handLandmarks[14].transform.position;
-        _fingerTargets[10].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[10].x, _fingerDirections[10].y, _fingerDirections[10].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[10].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[10].x, _fingerDirections[10].y, _fingerDirections[10].z)) * InitFingerRotation;
         _fingerDirections[11] = _handLandmarks[16].transform.position - _handLandmarks[15].transform.position;
-        _fingerTargets[11].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[11].x, _fingerDirections[11].y, _fingerDirections[11].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[11].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[11].x, _fingerDirections[11].y, _fingerDirections[11].z)) * InitFingerRotation;
 
         _fingerDirections[12] = _handLandmarks[18].transform.position - _handLandmarks[17].transform.position;
-        _fingerTargets[12].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[12].x, _fingerDirections[12].y, _fingerDirections[12].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[12].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[12].x, _fingerDirections[12].y, _fingerDirections[12].z)) * InitFingerRotation;
         _fingerDirections[13] = _handLandmarks[19].transform.position - _handLandmarks[18].transform.position;
-        _fingerTargets[13].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[13].x, _fingerDirections[13].y, _fingerDirections[13].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[13].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[13].x, _fingerDirections[13].y, _fingerDirections[13].z)) * InitFingerRotation;
         _fingerDirections[14] = _handLandmarks[20].transform.position - _handLandmarks[19].transform.position;
-        _fingerTargets[14].rotation = Quaternion.LookRotation(new Vector3(-_fingerDirections[14].x, _fingerDirections[14].y, _fingerDirections[14].z)) * Quaternion.Euler(90, 0, 0);
+        _fingerTargets[14].rotation = Quaternion.LookRotation(new Vector3(_fingerDirections[14].x, _fingerDirections[14].y, _fingerDirections[14].z)) * InitFingerRotation;
 
       }
     }
