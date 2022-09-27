577839dea94a3ab82b35e16079dfb7d51700d87b
superkudo <superkudo@163.com>
2022-09-27 11:25:11 +0800

    Fixed some conding style recommend

--- Assets/Src/Scripts/Animator/PoseLandmarksRecognizer.cs
+++ Assets/Src/Scripts/Animator/PoseLandmarksRecognizer.cs
@@ -77,7 +77,8 @@ namespace SeedUnityVRKit {
                                              rightShoulder - rightElbow, -forward) });
         landmarks.Add(new PoseLandmark { Id = Landmarks.LeftElbow,
                                          Rotation = Quaternion.LookRotation(
-                                             rightElbow - rightWrist, rightShoulder - rightElbow) });
+                                             rightElbow - rightWrist,
+                                             rightShoulder - rightElbow) });
         landmarks.Add(new PoseLandmark { Id = Landmarks.RightShoulder,
                                          Rotation = Quaternion.LookRotation(
                                              leftShoulder - leftElbow, -forward) });
